e50e2dc6c8e988e8788b46a180bcabc9c74d5473
ahelfouly <75209455+ahelfouly@users.noreply.github.com>
2021-06-09 19:31:12 +0200

    Update Main.m
    Added narrow band frequency modulation

--- Main.m
+++ Main.m
@@ -27,18 +27,20 @@ msg_t = real(ifft(ifftshift(Msg_f)));
 subplot(2,1,1);
 plot(tvec, msg_t); title('Filtered msg in t-domain');
 
+mean(Msg_f)
 sound(msg_t,fs);
 
 % NBFM mod
 % beta << 1 for NBFM
 fc = 100000; %10^5 Hz
-fs = 5 * fc;
-fDev = 50;
-figure;
-s_t = fmmod(msg_t, fc, fs, fDev);
-plot(tvec ,msg_t ,'c' ,tvec ,s_t ,'b--')
-
-figure;
-S_f = abs(fftshift(fft(s_t)));
-fvec = linspace(-fs/2,fs/2,length(S_f));
-plot(fvec, S_f); title('FM signal in Frequency Domain');
+fs_new = 5 * fc;
+msg_t_resam = resample(msg_t,fs_new,fs);
+tvec_new = linspace(0,length(msg_t_resam)/fs_new,length(msg_t_resam));
+carrier = 10*cos(2*pi*fc*tvec_new);
+sine = hilbert(carrier);
+phi = cumsum(msg_t_resam);
+s_t = carrier' - ( msg_t_resam .* sine' );
+s_f = abs(fftshift(fft(s_t)));
+fvec_new = linspace(-fs_new/2,fs_new/2,length(s_f));
+figure(3);
+plot(fvec_new, s_f); title('FM signal in Frequency Domain');
